0af32c1ae0199e0111ca4be06541d41f99674e02
Darii Yar <dariiy.yar@gmail.com>
2020-05-30 18:28:57 +0300

    Add Project price validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ApplicationRecord
   has_many :tasks
 
   validates :name, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: :tasks_price }
 
   def estimate_date
     tasks.order('tasks.estimate_date DESC').first.estimate_date unless tasks.empty?
@@ -18,5 +18,11 @@ class Project < ApplicationRecord
 
   def task_count
     tasks.count unless tasks.empty?
+  end
+
+  def tasks_price
+    return 0 if tasks.empty?
+    tasks_prices = tasks.map(&:price).reject(&:nil?)
+    tasks_prices.empty? ? 0 : tasks_prices.sum
   end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -19,4 +19,21 @@ RSpec.describe Project, type: :model do
     subject.description = FFaker::Lorem.sentence
     expect(subject).not_to be_valid
   end
+
+  it 'is not valid when price less then 0' do
+    subject.price = -1
+    expect(subject).not_to be_valid
+  end
+
+  context "price validator" do
+    subject { Project.first }
+    before(:each) do
+      create(:project_with_tasks)
+    end
+
+    it 'price should not be less then total tasks price' do
+      subject.price = subject.tasks_price - 1
+      expect(subject).not_to be_valid
+    end
+  end
 end
